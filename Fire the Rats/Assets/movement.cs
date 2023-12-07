fbe8cbca273c075c25e974b8d14e1ac46bdae3cf
pppeeern <kaoklongpeerawas@gmail.com>
2023-12-08 00:22:27 +0700

    First Create
    Added sprite (import from Tiny RPG - Forest)
    Imported New Input System
    Attempted to do the movement system

---
+++ Fire the Rats/Assets/movement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float Speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
